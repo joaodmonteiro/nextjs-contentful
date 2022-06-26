0e6988194d0f77178a3f0db3361e3acfe38d8903
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-06-26 12:47:41 +0100

    added getstaticpaths

--- pages/newsArticles/[articleID].jsx
+++ pages/newsArticles/[articleID].jsx
@@ -7,24 +7,63 @@ const client = contentful.createClient({
   accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
 });
 
-export default function Article({ title, body }) {
+export default function Article(props) {
+    if(props.error) {
+        return(
+            <div>
+                <h1>No article found</h1>
+            </div>
+        )
+    }
+    
     return(
         <div>
-            <h1>{title}</h1>
-            <div>{documentToReactComponents(body)}</div>
+            <h1>{props.title}</h1>
+            <div>{documentToReactComponents(props.body)}</div>
         </div>
     )
 }
 
-export async function getStaticProps(context) {
-    const resp = await client.getEntry(context.params.articleID);
+export async function getStaticPaths() {
+    const articles = await client.getEntries();
 
+    console.log(articles.items);
+
+    const paths = articles.items.map( article => ({
+        params: {
+            articleID: article.sys.id
+        }
+    }))
     return {
-        props: {
-            title: resp.fields.title,
-            body: resp.fields.body
-        },
-        revalidate: 60
+        fallback: false,
+        paths,
     }
+}
+
+export async function getStaticProps(context) {
+    let resp;
+    let errorMessage;
+    try {
+        resp = await client.getEntry(context.params.articleID);
+    } catch(err) {
+        errorMessage = err;
+    }
+    
+    if(resp) {
+        return {
+            props: {
+                title: resp.fields.title,
+                body: resp.fields.body
+            },
+        }
+    }
+    else {
+        return {
+            props: {
+                error: `Error: ${errorMessage}`
+            },
+        }
+    }
+    
 }
 
